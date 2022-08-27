3c0979ec4c5136e133a897e745605349bbe07a44
Sandra  Lima <sandralima.dev@gmail.com>
2022-08-26 23:40:38 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDsar1ABatm5VXFw6amRYmDXmU0inJpkWk",
+    authDomain: "lp-mundo-invertido.firebaseapp.com",
+    projectId: "lp-mundo-invertido",
+    storageBucket: "lp-mundo-invertido.appspot.com",
+    messagingSenderId: "29354123444",
+    appId: "1:29354123444:web:e99e964f702d3ec83ef7e1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default app
+  
